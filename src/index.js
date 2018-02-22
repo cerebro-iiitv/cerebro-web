d4d2d9a4ecc2622cf90723befbc597b75a91ce26
Pratyush Singh <singh.pratyush96@gmail.com>
2018-02-22 15:15:06 +0530

    Integrate firebase

--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@ import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 import Icons from 'uikit/dist/js/uikit-icons';
 import UIKit from 'uikit';
+import {firebase} from 'firebase';
 
 import registerServiceWorker from './registerServiceWorker';
 import {getStore} from "./store";
 
+// UIKit
 UIKit.use(Icons);
 
+// Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyDtC9Ob9VE2e5fVOrTD3a0khcBo2rRY8jM",
+  authDomain: "cerebro-iiitv-2018.firebaseapp.com",
+  databaseURL: "https://cerebro-iiitv-2018.firebaseio.com",
+  projectId: "cerebro-iiitv-2018",
+  storageBucket: "cerebro-iiitv-2018.appspot.com",
+  messagingSenderId: "765373525937"
+});
+
+// Store
 const store = getStore();
 
 ReactDOM.render(
