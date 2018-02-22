6f03ae28d7a88c76e8d48890a51fbcd30e9abe14
Pratyush Singh <singh.pratyush96@gmail.com>
2018-02-22 14:54:54 +0530

    Add initial redux and UIKit setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import Icons from 'uikit/dist/js/uikit-icons';
+import UIKit from 'uikit';
 
-ReactDOM.render(<div>Hello World</div>, document.getElementById('root'));
+import registerServiceWorker from './registerServiceWorker';
+import {getStore} from "./store";
+
+UIKit.use(Icons);
+
+const store = getStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+        Hello World!
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import {applyMiddleware, compose, createStore} from "redux";
+import thunk from "redux-thunk";
+import reducers from "../reducers";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = null;
+
+export const getStore = () => {
+  if (!store) {
+    store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+  }
+  return store;
+};
+
+export const getState = () => {
+  getStore().getState();
+};
