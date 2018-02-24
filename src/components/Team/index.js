5fce8869d31a9a5587c782864da6f3976bd4422f
Anshuman <anshu.av97@gmail.com>
2018-02-24 22:50:39 +0530

    Add team page with sample data

---
+++ src/components/Team/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import team from '../../data/team';
+
+class Team extends Component {
+	render() {
+		return(
+			<div className="team">
+				{team.map( person => (
+					<div class="uk-card uk-card-default team-card">
+						<div class="uk-card-media-top">
+							<img src={person.img} alt="person.name" />
+						</div>
+						<div class="uk-card-body">
+							<h3 class="uk-card-title">{person.name}</h3>
+							<p>{person.desc}</p>
+						</div>
+					</div>
+				))}
+			</div>
+		)
+	}
+}
+
+export default Team;
