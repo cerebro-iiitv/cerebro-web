b9b2b8fa7d8ecb44c3a3f393660a65a304024db3
Anshuman Verma <anshu.av97@gmail.com>
2018-02-25 17:59:00 +0530

    Fix travis build (#12)

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,18 +7,18 @@ class Navbar extends Component {
         <div class="uk-navbar-center">
 
           <ul class="uk-navbar-nav">
-            <li><a href="#">Timeline</a></li>
+            <li><a href="/">Timeline</a></li>
               <li>
-                <a href="#">Events</a>
+                <a href="/">Events</a>
                 <div class="uk-navbar-dropdown">
                   <ul class="uk-nav uk-navbar-dropdown-nav">
-                    <li class="uk-active"><a href="#">Technical</a></li>
-                    <li><a href="#">Fun</a></li>
-                    <li><a href="#">Gaming</a></li>
+                    <li class="uk-active"><a href="/">Technical</a></li>
+                    <li><a href="/">Fun</a></li>
+                    <li><a href="/">Gaming</a></li>
                   </ul>
                 </div>
               </li>
-            <li><a href="#">About Cerebro</a></li>
+            <li><a href="/">About Cerebro</a></li>
           </ul>
 
         </div>
