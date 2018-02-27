ea6377102a58858b5c011f5e8ccb3705308f1d29
Anshuman <anshu.av97@gmail.com>
2018-02-28 02:23:14 +0530

    Use className and update user details style

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -29,17 +29,17 @@ class Dashboard extends React.Component {
         <div className="dashboard-user-content">
           <div className="user-name">{this.props.user ? this.props.user.displayName : 'human'}.</div>
           <p><b>Mobile: </b>{this.props.userPhone}</p>
-          <button className="uk-button uk-flex-bottom uk-button-secondary" onClick={() => firebase.auth().signOut()}>Sign out</button>
+          <button className="uk-button uk-button-secondary" onClick={() => firebase.auth().signOut()}>Sign out</button>
         </div>
       </div>
       <h3>Registered Events</h3>
-      <table class="uk-table uk-table-middle uk-table-divider">
+      <table className="uk-table uk-table-middle uk-table-divider">
         <thead>
             <tr>
               <th>#</th>
               <th>Event Name</th>
-              <th class="uk-width-small">Starts On</th>
-              <th class="uk-width-small">Action</th>
+              <th className="uk-width-small">Starts On</th>
+              <th className="uk-width-small">Action</th>
             </tr>
         </thead>
         <tbody>
@@ -48,13 +48,13 @@ class Dashboard extends React.Component {
                 <td>{event.id}</td>
                 <td>{event.name}</td>
                 <td>-</td>
-                <td><button className="uk-button primary-color primary-color-border uk-button-default" type="button" onClick={() => this.props.unregisterFromEvent(event)}>Unregister</button></td>
+                <td><button className="uk-button uk-button-secondary primary-color primary-color-border uk-button-small" onClick={() => this.props.unregisterFromEvent(event)}>Unregister</button></td>
             </tr>
           )}
         </tbody>
       </table>
       <div className="update-mobile">
-        <input ref={ref => this.mobileInput = ref} class="uk-input uk-form-width-medium" type="text" placeholder="Medium" type={'phone'} id={'phone-input'} placeholder={'Mobile'} />
+        <input ref={ref => this.mobileInput = ref} className="uk-input uk-form-width-medium" type="text" placeholder="Medium" type={'phone'} id={'phone-input'} placeholder={'Mobile'} />
         <button className="uk-button uk-button-secondary" onClick={() => {this.props.saveUserPhone(this.mobileInput.value); this.mobileInput.value=''}}>Save</button>
       </div>
     </div>
