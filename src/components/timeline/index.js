0d05d44e1b720a5f68298b300a78cd67211ba062
Pratyush Singh <singh.pratyush96@gmail.com>
2018-02-27 10:51:47 +0530

    Prevent mutation of state while copying events in Timeline

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 
 class Timeline extends React.Component {
   render() {
-    let events = this.props.event.events;
+    let events = this.props.event.events.slice();
     events.sort((a, b) => (Date.parse(a['start-time']) - Date.parse(b['start-time'])));
     return <div>
       {events.map(event => <li key={event.id}>{event.name}</li>)}
