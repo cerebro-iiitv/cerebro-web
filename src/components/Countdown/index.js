f04e510fb85f7199c5107de63461305c7ce56437
Pratyush Singh <singh.pratyush96@gmail.com>
2018-02-28 01:45:27 +0530

    Fix countdown styling

--- src/components/Countdown/index.js
+++ src/components/Countdown/index.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
 
+import heroImage from './cerebro-hero.png';
+import './style.css';
+
 class Countdown extends Component {
 	render() {
 		return(
 			<div className="countdown">
-				<h1 className="countdown-header">It begins in - </h1>
+        <img className={'uk-margin-large-bottom cerebro-hero'} src={heroImage} />
+        <hr style={{width: '260px'}}/>
+				<span className="countdown-header">STARTS IN</span>
 				<div className="uk-grid-small uk-child-width-auto timer" uk-grid="true" uk-countdown="date: 2018-03-09T05:07:04+00:00">
 					<div>
 						<div className="uk-countdown-number uk-countdown-days"></div>
-						<div className="uk-countdown-label uk-margin-small uk-text-center uk-visible@s">Days</div>
+						<div className="uk-countdown-label uk-text-center uk-visible@s">DAYS</div>
 					</div>
 					<div className="uk-countdown-separator">:</div>
 					<div>
 						<div className="uk-countdown-number uk-countdown-hours"></div>
-						<div className="uk-countdown-label uk-margin-small uk-text-center uk-visible@s">Hours</div>
+						<div className="uk-countdown-label uk-text-center uk-visible@s">HRS</div>
 					</div>
 					<div className="uk-countdown-separator">:</div>
 					<div>
 						<div className="uk-countdown-number uk-countdown-minutes"></div>
-						<div className="uk-countdown-label uk-margin-small uk-text-center uk-visible@s">Minutes</div>
+						<div className="uk-countdown-label uk-text-center uk-visible@s">MINS</div>
 					</div>
 					<div className="uk-countdown-separator">:</div>
 					<div>
 						<div className="uk-countdown-number uk-countdown-seconds"></div>
-						<div className="uk-countdown-label uk-margin-small uk-text-center uk-visible@s">Seconds</div>
+						<div className="uk-countdown-label uk-text-center uk-visible@s">SECS</div>
 					</div>
 				</div>
 			</div>
